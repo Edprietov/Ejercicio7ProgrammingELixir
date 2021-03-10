539ba519800698261ea0889607cc518c0cddf2d4
Edisson <edprietov@unal.edu.co>
2021-03-09 19:10:45 -0500

    Inicio del Proyecto

---
+++ organizando/lib/organizando.ex
@@ -0,0 +1,18 @@
+defmodule Organizando do
+  @moduledoc """
+  Documentation for `Organizando`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Organizando.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ organizando/test/organizando_test.exs
@@ -0,0 +1,8 @@
+defmodule OrganizandoTest do
+  use ExUnit.Case
+  doctest Organizando
+
+  test "greets the world" do
+    assert Organizando.hello() == :world
+  end
+end
